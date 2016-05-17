7a1ceaf8dcd32874ac9e639bd64cf496e67bde0e
Alex Kad <Alexandra_Kadushkina@epam.com>
2016-05-17 16:04:44 +0300

    Task description added

--- permMissingElem/solution.js
+++ permMissingElem/solution.js
@@ -1,4 +1,7 @@
-// score 100%
+// A zero-indexed array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+// Your goal is to find that missing element.
+//
+//100% score solution
 function solution(A) {
     var n = A.length,
     sum = (n+1)*(n+2)/2;
